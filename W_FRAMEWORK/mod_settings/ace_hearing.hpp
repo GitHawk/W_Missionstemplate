24ca4e3bc19749d184d875d06b2164e701579149
Felix Linker <felixlinker@hotmail.de>
2015-11-17 20:10:14 +0100

    Hotfix

--- W_FRAMEWORK/mod_settings/ace_hearing.hpp
+++ W_FRAMEWORK/mod_settings/ace_hearing.hpp
@@ -1,8 +1,8 @@
-class GVAR(EnableCombatDeafness) {
+class ace_hearing_EnableCombatDeafness {
 	value = 0;
 	typeName = "BOOL";
 };
-class GVAR(DisableEarRinging) {
+class ace_hearing_DisableEarRinging {
 	value = 1;
 	typeName = "BOOL";
 	isClientSettable = 1;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-﻿# W_Missionstemplate 3v30
+﻿# W_Missionstemplate 3v31
 
 Lieber Missionsbauer,
 in dieser kleinen Datei möchte ich euch ein paar Grundzüge des Missionsbau und dieses Templates näherbringen.
