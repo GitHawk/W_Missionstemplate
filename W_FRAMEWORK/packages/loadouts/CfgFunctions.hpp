a518a4306392f8ac20243404105fffcffb81b121
Githawk <github@ingokauffmann.de>
2016-02-09 17:33:24 +0100

    Added api draft

--- W_FRAMEWORK/packages/loadouts/CfgFunctions.hpp
+++ W_FRAMEWORK/packages/loadouts/CfgFunctions.hpp
@@ -5,3 +5,20 @@ class framework_loadouts {
 		class applyLoadout {};
 	};
 };
+class framework_loadouts_api {
+	tag = "FETT";
+	class main {
+		file = "W_FRAMEWORK\packages\loadouts\functions";
+        class west_loadout {};
+        class b_loadout {};
+
+        class east_loadout {};
+        class o_loadout {};
+
+        class independent_loadout {};
+		class i_loadout {};
+
+        class civilian_loadout {};
+		class c_loadout {};
+	};
+};
