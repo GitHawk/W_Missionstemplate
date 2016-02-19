4b032e9d55ba9cb6ea2194cac5c66dfa03bb7d61
Alexander Wolf <lexi_wolf@t-online.de>
2016-02-19 22:10:43 +0100

    Eden-Loadout fix

--- W_FRAMEWORK/packages/loadouts/CfgFramework.hpp
+++ W_FRAMEWORK/packages/loadouts/CfgFramework.hpp
@@ -1,3 +1,9 @@
+class loadoutsFix {
+	code = "[] call FETT_framework_fnc_serverCreateHash";
+	server = 1;
+	preinit = 1;
+};
+
 class loadouts {
 	code = "[player] call FETT_framework_fnc_applyLoadout";
 	client = 1;
